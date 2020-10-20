990f65c58a5f029c4756726f1aae668b359f0bc3
Yifan Pan <4anpan@gmail.com>
2020-10-20 11:48:46 -0700

    change uri

--- src/graphql-client.js
+++ src/graphql-client.js
@@ -1,9 +1,16 @@
 import gql from 'graphql-tag';
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
+let uri;
+if (process.env.NODE_ENV == 'development') {
+  uri = 'http://localhost:8080/graphql';
+} else {
+  uri = `https://${location.host}/api/memorizing/graphql`;
+}
+
 const client = new ApolloClient({
   link: createHttpLink({
-    uri: 'http://localhost:8080/graphql',
+    uri,
     credentials: 'include'
   }),
   cache: new InMemoryCache(),
